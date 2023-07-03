f60c9842275e40886dcf46c22e1604d516a528de
Hakan <90573332+HakanBabus@users.noreply.github.com>
2023-07-03 21:36:26 +0300

    Add files via upload

--- src/HakanBabus/EasyAnnouncement/EasyAnnouncement.php
+++ src/HakanBabus/EasyAnnouncement/EasyAnnouncement.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace HakanBabus\EasyAnnouncement;
 
+use HakanBabus\EasyAnnouncement\commands\AnnouncementCommand;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\lang\Translatable;
@@ -30,36 +31,17 @@ class EasyAnnouncement extends PluginBase{
             }), $ao["time"]*20);
         }
         if(($pa = $this->getConfig()->get("player-announcement"))["enabled"]){
-            $this->getServer()->getCommandMap()->register("hb", new class(
-                $this,
-                $pa,
-                $pa["command-settings"]["name"],
-                $pa["command-settings"]["description"],
-                $pa["command-settings"]["usage"],
-                $pa["command-settings"]["aliases"]
-            ) extends Command{
-                public function __construct(public EasyAnnouncement $main, public $cmdarray, string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = [])
-                {
-                    parent::__construct($name, $description, $usageMessage, $aliases);
-                    $this->setPermission("announcement.command");
-                }
-
-                public function execute(CommandSender $sender, string $commandLabel, array $args)
-               {
-                   if($this->testPermission($sender)){
-                       if(isset($args[0])){
-                           Server::getInstance()->broadcastMessage(str_replace("{player}", $sender->getName(), $this->cmdarray["prefix"]). implode(" ", $args));
-                           if($this->cmdarray["sound"]["enabled"]){
-                               foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
-                                   $this->main->playSound($onlinePlayer->getPosition(), $onlinePlayer);
-                               }
-                           }
-                           return;
-                       }
-                       $sender->sendMessage($this->getUsage());
-                   }
-               }
-            });
+            $this->getServer()->getCommandMap()->register(
+                "hb", 
+                new AnnouncementCommand(
+                    $this,
+                    $pa,
+                    $pa["command-settings"]["name"],
+                    $pa["command-settings"]["description"],
+                    $pa["command-settings"]["usage"],
+                    $pa["command-settings"]["aliases"]
+                )
+            );
         }
     }
     
