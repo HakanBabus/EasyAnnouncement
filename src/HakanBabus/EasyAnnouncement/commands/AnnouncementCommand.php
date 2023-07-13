96c48f9e7f5cbe8793790ce09b29fa3a186bbdfd
Hakan <90573332+HakanBabus@users.noreply.github.com>
2023-07-13 20:27:51 +0300

    cs

--- src/HakanBabus/EasyAnnouncement/commands/AnnouncementCommand.php
+++ src/HakanBabus/EasyAnnouncement/commands/AnnouncementCommand.php
@@ -10,25 +10,25 @@ use pocketmine\Server;
 
 class AnnouncementCommand extends Command
 {
-        public function __construct(public EasyAnnouncement $main, public $cmdarray, string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = [])
-                {
-                    parent::__construct($name, $description, $usageMessage, $aliases);
-                    $this->setPermission("easyannouncement.command");
-                }
+    public function __construct(public EasyAnnouncement $main, public $cmdarray, string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = [])
+        {
+            parent::__construct($name, $description, $usageMessage, $aliases);
+            $this->setPermission("easyannouncement.command");
+        }
 
-        public function execute(CommandSender $sender, string $commandLabel, array $args)
-               {
-                   if($this->testPermission($sender)){
-                       if(isset($args[0])){
-                           Server::getInstance()->broadcastMessage(str_replace("{player}", $sender->getName(), $this->cmdarray["prefix"]). implode(" ", $args));
-                           if($this->cmdarray["sound"]["enabled"]){
-                               foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
-                                   $this->main->playSound($onlinePlayer->getPosition(), $onlinePlayer);
-                               }
-                           }
-                           return;
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+        {
+            if($this->testPermission($sender)){
+               if(isset($args[0])){
+                   Server::getInstance()->broadcastMessage(str_replace("{player}", $sender->getName(), $this->cmdarray["prefix"]). implode(" ", $args));
+                    if($this->cmdarray["sound"]["enabled"]){
+                       foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
+                           $this->main->playSound($onlinePlayer->getPosition(), $onlinePlayer);
                        }
-                       $sender->sendMessage($this->getUsage());
-                   }
-               }
+                    }
+                    return;
+                }
+                $sender->sendMessage($this->getUsage());
+            }
+        }
 }
