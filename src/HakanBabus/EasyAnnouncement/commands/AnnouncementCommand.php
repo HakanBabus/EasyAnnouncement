46927ea32c3b0f2174189fa393ddc2269d1f21ec
HakanBabus <hakangame08@gmail.com>
2023-07-16 12:41:52 +0300

    restore plugin

--- src/HakanBabus/EasyAnnouncement/commands/AnnouncementCommand.php
+++ src/HakanBabus/EasyAnnouncement/commands/AnnouncementCommand.php
@@ -6,29 +6,41 @@ use HakanBabus\EasyAnnouncement\EasyAnnouncement;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\lang\Translatable;
+use pocketmine\plugin\PluginOwned;
 use pocketmine\Server;
 
-class AnnouncementCommand extends Command
+class AnnouncementCommand extends Command implements PluginOwned
 {
-    public function __construct(public EasyAnnouncement $main, public $cmdarray, string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = [])
-                {
-                    parent::__construct($name, $description, $usageMessage, $aliases);
-                    $this->setPermission("announcement.command");
-                }
+    public function __construct(
+        public EasyAnnouncement $main, 
+        public $cmdarray, string $name, 
+        Translatable|string $description = "", 
+        Translatable|string|null $usageMessage = null, 
+        array $aliases = []
+    )
+    {
+        parent::__construct($name, $description, $usageMessage, $aliases);
+        $this->setPermission("easyannouncement.command");
+    }
 
-                public function execute(CommandSender $sender, string $commandLabel, array $args)
-               {
-                   if($this->testPermission($sender)){
-                       if(isset($args[0])){
-                           Server::getInstance()->broadcastMessage(str_replace("{player}", $sender->getName(), $this->cmdarray["prefix"]). implode(" ", $args));
-                           if($this->cmdarray["sound"]["enabled"]){
-                               foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
-                                   $this->main->playSound($onlinePlayer->getPosition(), $onlinePlayer);
-                               }
-                           }
-                           return;
-                       }
-                       $sender->sendMessage($this->getUsage());
-                   }
-               }
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($this->testPermission($sender)){
+            if(isset($args[0])){
+                Server::getInstance()->broadcastMessage(str_replace("{player}", $sender->getName(), $this->cmdarray["prefix"]). implode(" ", $args));
+                if($this->cmdarray["sound"]["enabled"]){
+                     foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
+                         $this->main->playSound($onlinePlayer->getPosition(), $onlinePlayer);
+                    }
+                }
+                return;
+            }
+            $sender->sendMessage($this->getUsage());
+        }
+    }
+
+    public function getOwningPlugin(): EasyAnnouncement
+    {
+        return $this->main;
+    }
 }
